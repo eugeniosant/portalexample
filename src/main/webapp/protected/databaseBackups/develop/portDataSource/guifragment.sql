270b45a1ad7369fb38c7a187ea914a0449b9390e
eugeniosant <eugenio.santoboni@gmail.com>
2014-10-09 11:40:23 +0200

    jacms: Add new widget - "Publish contents"

--- src/main/webapp/protected/databaseBackups/develop/portDataSource/guifragment.sql
+++ src/main/webapp/protected/databaseBackups/develop/portDataSource/guifragment.sql
@@ -2357,3 +2357,32 @@ INSERT INTO guifragment (code, widgettypecode, plugincode, gui, defaultgui, lock
 <#else>
 <p class="alert alert-info"><@wp.i18n key="SEARCH_NOTHING_FOUND" /></p>
 </#if>', NULL, 0);
+INSERT INTO guifragment (code, widgettypecode, plugincode, gui, defaultgui, locked) VALUES ('jacms_row_content_viewer_list', 'row_content_viewer_list', 'jacms', NULL, '<#assign jacms=JspTaglibs["/jacms-aps-core"]>
+<#assign c=JspTaglibs["http://java.sun.com/jsp/jstl/core"]>
+<#assign wp=JspTaglibs["/aps-core"]>
+<@jacms.rowContentList listName="contentInfoList" titleVar="titleVar"
+	pageLinkVar="pageLinkVar" pageLinkDescriptionVar="pageLinkDescriptionVar" />
+<#if (titleVar??)>
+	<h1><@c.out value="${titleVar}" /></h1>
+</#if>
+<#if (contentInfoList??) && (contentInfoList?has_content) && (contentInfoList?size > 0)>
+	<@wp.pager listName="contentInfoList" objectName="groupContent" pagerIdFromFrame=true advanced=true offset=5>
+	<@wp.freemarkerTemplateParameter var="group" valueName="groupContent" removeOnEndTag=true >
+	<@wp.fragment code="default_pagerBlock" escapeXml=false />
+	<#list contentInfoList as contentInfoVar>
+	<#if (contentInfoVar_index >= groupContent.begin) && (contentInfoVar_index <= groupContent.end)>
+		<#if (contentInfoVar[''modelId'']??)>
+		<@jacms.content contentId="${contentInfoVar[''contentId'']}" modelId="${contentInfoVar[''modelId'']}" />
+		<#else>
+		<@jacms.content contentId="${contentInfoVar[''contentId'']}" />
+		</#if>
+	</#if>
+	</#list>
+	<@wp.fragment code="default_pagerBlock" escapeXml=false />
+	</@wp.freemarkerTemplateParameter>
+	</@wp.pager>
+</#if>
+<#if (pageLinkVar??) && (pageLinkDescriptionVar??)>
+	<p class="text-right"><a class="btn btn-primary" href="<@wp.url page="${pageLinkVar}"/>"><@c.out value="${pageLinkDescriptionVar}" /></a></p>
+</#if>
+<@c.set var="contentInfoList" scope="request" />', 1);
